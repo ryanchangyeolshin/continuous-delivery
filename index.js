139bfc2910eac74cde857f9e240a548c8053e9fb
Ryan Shin <changyeolshin@gmail.com>
2017-10-30 11:03:34 -0700

    Handled errors.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { MongoClient } = require('mongodb')
 const createApp = require('./create-app')
 
 MongoClient.connect(process.env.MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
   createApp(db)
     .listen(process.env.PORT, () => console.log('Listening to PORT' + process.env.PORT))
 })
