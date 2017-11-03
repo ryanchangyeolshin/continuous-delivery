16b87df30c5634f310ba01ac8050768790f65e6a
Ryan Shin <changyeolshin@gmail.com>
2017-11-03 14:17:39 -0700

    Changed the TodoForm component from a class to a function. #9

--- client/todo-form.js
+++ client/todo-form.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class TodoForm extends Component {
-  constructor(props) {
-    super(props)
-    this.props = props
-  }
-
-  render() {
-    return (
-      <form className="mt-lg-4" onSubmit={this.props.handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="dueDate">Due Date</label>
-          <input type="date" className="form-control" name="dueDate" id="dueDate" placeholder="Enter due date." />
-        </div>
-        <div className="form-group">
-          <label htmlFor="task">Task</label>
-          <input type="text" className="form-control" name="task" id="task" placeholder="Please enter task." />
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-    )
-  }
+export default function TodoForm({ handleSubmit }) {
+  return (
+    <form className="mt-lg-4" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="dueDate">Due Date</label>
+        <input type="date" className="form-control" name="dueDate" id="dueDate" placeholder="Enter due date." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="task">Task</label>
+        <input type="text" className="form-control" name="task" id="task" placeholder="Please enter task." />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  )
 }
